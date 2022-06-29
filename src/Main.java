643a6287e53eab7d4fceda3f5897bdc910a8fa91
Birivaty <birivaty@yandex.ru>
2022-06-29 21:32:50 +0300

    new

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int bonus;
+        int added = 1001
+                ;
+        if (added <= 1000) {
+            bonus = 0;
+        } else {
+            bonus = added / 100;
+
+        }
+        int finish_balance = balance + added + bonus;
+        System.out.println("Баланс" + finish_balance);
+    }
+}
